df8e828d73bb6a005cef92353b0e2a2fc8228dd8
Ignacio <nosoyignacio@gmail.com>
2024-01-02 18:33:05 -0300

    animated & functional contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,22 @@
-import React, { useRef, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import emailjs from "@emailjs/browser"
+import Fox from "../models/Fox"
+import Loader from "../components/Loader"
+import { Canvas } from "@react-three/fiber";
+import useAlert from "../hooks/useAlert";
 
 function Contact() {
 
   const formRef = useRef(null)
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setisLoading] = useState(false)
+  const [currentAnimation, setcurrentAnimation] = useState("idle")
+  const {alert, showAlert, hideAlert} = useAlert()
 
   const handleSubmit = (e) => {
    e.preventDefault()
    setisLoading(true)
-
+   setcurrentAnimation("walk")
    emailjs.send(
     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
@@ -24,24 +30,23 @@ function Contact() {
     import.meta.env.VITE_APP_EMAILJS_KEY
    ).then(() => {
     setisLoading(false)
-    //TO DO: Show succes message
-    //TO DO: Hide alert
-
+    showAlert({show: true, text: "Message sent successfuly!", type: "succes"})
+    setTimeout(() => {
+      setcurrentAnimation("idle")
+      setForm({name: "", email: "", message: ""})
+    }, [3000]);
    }).catch((er) => {
     setisLoading(false)
+    setcurrentAnimation("idle")
     console.log(er)
-    //TO DO: Show error
+    showAlert({show: true, text: "Something went wrong, try again later", type: "danger"})
    })
   }
   const handleChange = (e) => {
     setForm({...form, [e.target.name]: e.target.value})
   }
-  const handleFocus = () => {
-    
-  }
-  const handleBlur = () => {
-    
-  }
+  const handleFocus = () => setcurrentAnimation("walk")
+  const handleBlur = () => setcurrentAnimation("idle")
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
@@ -74,7 +79,7 @@ function Contact() {
               type='email'
               name='email'
               className='input'
-              placeholder='John@gmail.com'
+              placeholder='Your@mail.com'
               required
               value={form.email}
               onChange={handleChange}
@@ -106,6 +111,26 @@ function Contact() {
             {isLoading? "Sending..." : "Send Message"}
           </button>
         </form>
+      </div>
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h:[350px]">
+          <Canvas
+          camera={{
+            position: [0,0,5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}
+          >
+            <directionalLight intensity={2.5} position={[0,0,1]}/>
+            <Suspense fallback={<Loader/>}>
+              <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5,0.35,0]}
+              rotation={[12.6,-0.6,0]}
+              scale={[0.5,0.5,0.5]}
+              />
+            </Suspense>
+          </Canvas>
       </div>
       </section>
   );
